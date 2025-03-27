a3f7c560b5090944d38303b9e2ac5954944b67de
Jacky Cheng <j.cheng0915@gmail.com>
2025-03-27 03:41:38 -0400

    Reverting HashRouter change.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
