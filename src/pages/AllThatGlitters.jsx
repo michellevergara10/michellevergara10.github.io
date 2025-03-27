5b5bcc6f7a18875169522a0dfa11e789c5a11622
Jacky Cheng <j.cheng0915@gmail.com>
2025-03-27 04:47:48 -0400

    Updates

--- src/pages/AllThatGlitters.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from "react";
-import SectionBase from "./SectionBase";
-
-function AllThatGlitters() {
-  return SectionBase("allThatGlitters");
-}
-
-export default AllThatGlitters;
