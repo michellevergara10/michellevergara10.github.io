afc321f7fccfe70f121a35020e4828baab9ddfc8
Jacky Cheng <j.cheng0915@gmail.com>
2025-04-04 03:08:30 -0400

    Updates

--- src/pages/TheGiftOfLosAngeles.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from "react";
-import SectionBase from "./SectionBase";
-
-function TheGiftOfLosAngeles() {
-  return SectionBase("theGiftOfLosAngeles");
-}
-
-export default TheGiftOfLosAngeles;
